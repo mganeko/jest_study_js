9ecb29bc987e6f153bfaf9873ca2b22928e5d7a4
masashi.g <masashi.g@gmail.com>
2021-04-12 10:23:56 +0900

    remove not used code

--- __tests__/intClosedRange.test.js
+++ __tests__/intClosedRange.test.js
@@ -11,6 +11,7 @@ test('必ず成功', () => {
   expect(1).toBe(1);
 });
 
+// --- test for setup --
 // test('必ず失敗', () => {
 //   expect(true).toBe(false);
 // });
@@ -27,46 +28,13 @@ describe('整数閉区間IntClosedRange-[3,7]の場合', () => {
   });
 
   test('上端点getUpper()は7', () => {
-    //const range = new IntClosedRange(3, 7);
     expect(range.getUpper()).toBe(7);
   });
 
 
   test('notation()は文字列表記"[3,7]"を返す', () => {
-    //const range = new IntClosedRange(3, 7);
     expect(range.notation()).toBe('[3,7]');
   });
-
-  // test('整数閉区間は指定した整数を含むかどうかを返す', () => {
-  //   expect(range.includes(5)).toBe(true);
-  //   expect(range.includes(1)).toBe(false);
-  //   expect(range.includes(9)).toBe(false);
-
-  //   expect(range.includes(3)).toBe(true);
-  //   expect(range.includes(7)).toBe(true);
-  // });
-
-  // describe('includes()は指定した整数を含むかどうかを返す', () => {
-  //   test('5を含む。trueを返す', () => {
-  //     expect(range.includes(5)).toBe(true);
-  //   });
-
-  //   test('2を含まない。falseを返す', () => {
-  //     expect(range.includes(2)).toBe(false);
-  //   });
-
-  //   test('8を含まない。falseを返す', () => {
-  //     expect(range.includes(8)).toBe(false);
-  //   });
-
-  //   test('下端点3を含む。trueを返す', () => {
-  //     expect(range.includes(3)).toBe(true);
-  //   });
-
-  //   test('上端点7を含む。trueを返す', () => {
-  //     expect(range.includes(7)).toBe(true);
-  //   });
-  // });
 
   // パラメタライズドテスト
   describe('includes()は指定した整数を含むかどうかを返す', () => {
